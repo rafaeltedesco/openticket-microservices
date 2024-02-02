5f4e3e9620384ba6a2eebe5056b3b59df9319d3b
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2024-02-02 20:28:44 -0300

    feat: retrieve error if service is unavailable

--- api-gateway/api_gateway/routers/catalog.py
+++ api-gateway/api_gateway/routers/catalog.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
 from api_gateway.services import catalog as catalog_service
 
 catalog_router = APIRouter()
 
-@catalog_router.get("/", tags=["catalog"])
-async def show_catalog():
-  return catalog_service.fetch_catalog()
+@catalog_router.get("/", status_code=200)
+async def show_catalog(response: Response):
+  result = catalog_service.fetch_catalog()
+  if "error" in result:
+    response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
+  return result
+  
--- api-gateway/api_gateway/services/catalog.py
+++ api-gateway/api_gateway/services/catalog.py
@@ -3,7 +3,10 @@ import httpx, os
 CATALOG_SERVICE = os.getenv("CATALOG_SERVICE", "http://localhost:3030")
 
 def fetch_catalog():
-    response = httpx.get(f'{CATALOG_SERVICE}/catalog')
-    if (response.status_code != 200):
+    try:
+      response = httpx.get(f'{CATALOG_SERVICE}/catalog')
+      return response.json()
+    except httpx.RequestError:
         return {"error": "Catalog Service is not available"}
-    return response.json()
+    
+    
