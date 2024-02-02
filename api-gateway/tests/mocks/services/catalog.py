71bf81bbd6cde0fe72242ab7928aea9ce675ed8f
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2024-02-02 20:39:43 -0300

    test: add router test for catalog service unavailable

--- api-gateway/tests/mocks/services/catalog.py
+++ api-gateway/tests/mocks/services/catalog.py
@@ -1,1 +1,2 @@
 mock_fetch_catalog = [{"id": 1, "name": "Show da Xuxa", "eventDateTime": "2024-05-22T00:00:00"}]
+mock_error_unavailable = {"error": "Catalog Service is not available"}
--- api-gateway/tests/routers/test_catalog.py
+++ api-gateway/tests/routers/test_catalog.py
@@ -8,3 +8,9 @@ def test_show_catalog():
     response = client.get('/catalog')
   assert response.status_code == 200
   assert response.json() == [{"id": 1, "name": "Show da Xuxa", "eventDateTime": "2024-05-22T00:00:00"}]
+
+def test_service_unavailable():
+  with patch('api_gateway.services.catalog.fetch_catalog', return_value=mock.mock_error_unavailable):
+    response = client.get('/catalog')
+    assert response.status_code == 503
+    assert response.json() == {"error": "Catalog Service is not available"}
