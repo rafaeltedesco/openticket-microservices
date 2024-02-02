53dff82b5a21694f2fbb54bca639bb80f1a71776
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2024-02-02 20:22:48 -0300

    feat: request to catalog ms

--- api-gateway/api_gateway/services/catalog.py
+++ api-gateway/api_gateway/services/catalog.py
@@ -1,2 +1,9 @@
+import httpx, os
+
+CATALOG_SERVICE = os.getenv("CATALOG_SERVICE", "http://localhost:3030")
+
 def fetch_catalog():
-  return [{"id": 1, "name": "show 1"}, {"id": 2, "name": "show 2"}]
+    response = httpx.get(f'{CATALOG_SERVICE}/catalog')
+    if (response.status_code != 200):
+        return {"error": "Catalog Service is not available"}
+    return response.json()
