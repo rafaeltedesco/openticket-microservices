5b8a6b9e1dc83c80ed1bfb6060ec39ab62583d59
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2024-02-02 22:14:09 -0300

    fix: call to catalogservice inside docker

--- api-gateway/api_gateway/services/catalog.py
+++ api-gateway/api_gateway/services/catalog.py
@@ -1,12 +1,13 @@
 import httpx, os
 
-CATALOG_SERVICE = os.getenv("CATALOG_SERVICE", "http://localhost:3030")
+CATALOG_SERVICE_URL = os.getenv("CATALOG_SERVICE_URL", "http://localhost:3030")
 
 def fetch_catalog():
     try:
-      response = httpx.get(f'{CATALOG_SERVICE}/catalog')
+      response = httpx.get(f'{CATALOG_SERVICE_URL}/catalog')
       return response.json()
-    except httpx.RequestError:
+    except httpx.RequestError as e:
+        print("Request error", e)
         return {"error": "Catalog Service is not available"}
     
     
