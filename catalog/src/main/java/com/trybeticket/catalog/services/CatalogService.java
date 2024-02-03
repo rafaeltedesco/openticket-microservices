ef9352c28d7e245da10f6ea8a74c3ec5c5df4e48
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2024-02-02 21:46:59 -0300

    feat: create event

--- catalog/src/main/java/com/trybeticket/catalog/services/CatalogService.java
+++ catalog/src/main/java/com/trybeticket/catalog/services/CatalogService.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.trybeticket.catalog.dtos.CatalogRequestDto;
 import com.trybeticket.catalog.dtos.CatalogResponseDto;
+import com.trybeticket.catalog.entities.CatalogEvent;
+import com.trybeticket.catalog.entities.exceptions.InvalidDateException;
 import com.trybeticket.catalog.repositories.CatalogRepository;
 
 @Service
@@ -20,5 +23,13 @@ public class CatalogService {
   public List<CatalogResponseDto> getAvailableCatalogEvents(LocalDateTime when) {
     return catalogRepository.findByEventDateTimeAfter(when)
       .stream().map(CatalogResponseDto::fromEntity).toList();
+  }
+
+  public CatalogResponseDto createEvent(CatalogRequestDto eventCreationDto) {
+    CatalogEvent event = CatalogEvent.fromDto(eventCreationDto);
+    if (event.isValidateDate() == false) {
+      throw new InvalidDateException("Event date must be in the future");
+    }
+    return CatalogResponseDto.fromEntity(catalogRepository.save(event));
   }
 }
