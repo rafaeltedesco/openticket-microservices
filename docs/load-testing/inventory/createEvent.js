63ea605d19c8184353356dc8ebc8ac3775e43dbf
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2024-02-08 10:26:45 -0300

    feat: load testing add faker and uuid

--- docs/load-testing/inventory/createEvent.js
+++ docs/load-testing/inventory/createEvent.js
@@ -1,20 +1,15 @@
 const autocannon = require('autocannon');
 const { faker } = require('@faker-js/faker');
+const uuid = require('uuid');
 
-function generateRandomEvent() {
-  return {
-    name: faker.word.words(3),
-    eventDateTime: faker.date.future(),
-    ticketsAvailable: faker.number.int(100, 10000),
-  };
-}
+const initialEvent = generateRandomEvent();
 
 const options = {
   url: 'http://localhost/inventory/',
   method: 'POST',
-  connections: 15,
-  duration: 10,
-  body: JSON.stringify(generateRandomEvent()),
+  connections: 40,
+  duration: 60,
+  body: JSON.stringify(initialEvent),
   headers: {
     'Content-Type': 'application/json',
   },
@@ -22,22 +17,31 @@ const options = {
 
 const instance = autocannon(options);
 
-instance.on('response', (client, statusCode, resBytes) => {
+instance.on('response', (client, statusCode, _resBytes) => {
   console.log(`Got response with status code: ${statusCode}`);
-  
-  options.body = JSON.stringify(generateRandomEvent());
-  console.log(options.body);
-  client.setHeaders('content-length', Buffer.byteLength(options.body));
-  client.setBody(options.body);
-})
+
+  const randomEvent = generateRandomEvent();
+  const requestBody = JSON.stringify(randomEvent);
+
+  console.log(requestBody);
+  client.setHeaders('content-length', Buffer.byteLength(requestBody));
+  client.setBody(requestBody);
+});
 
 autocannon.track(instance, { renderProgressBar: true, renderLatencyTable: true });
 
 process.on('SIGINT', () => {
   autocannon.destroy(instance);
-})
+});
 
 process.on('SIGTERM', () => {
   autocannon.destroy(instance);
-})
+});
 
+function generateRandomEvent() {
+  return {
+    name: faker.word.words(2) + uuid.v4(),
+    eventDateTime: faker.date.future(),
+    ticketsAvailable: faker.number.int(100, 10000),
+  };
+}
