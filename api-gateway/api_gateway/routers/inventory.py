24bad1f2c63573fcf0a14dee1e3dc8ccb1d1cd5a
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2024-02-08 10:25:41 -0300

    feat: tweak to return the correct response

--- api-gateway/api_gateway/routers/inventory.py
+++ api-gateway/api_gateway/routers/inventory.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Response, status
+from fastapi import APIRouter, Response
 
 from api_gateway.dtos.inventory_event_dto import InventoryEventCreationDto
 from api_gateway.services import inventory as inventory_service
@@ -7,14 +7,13 @@ inventory_router = APIRouter()
 
 @inventory_router.post("/", status_code=201)
 async def add_item(event: InventoryEventCreationDto, response: Response):
-  result = inventory_service.add_event(event)
-  if "error" in result:
-    response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
-  return result
+  status, message = await inventory_service.add_event(event)
+  response.status_code = status
+  return message
 
 @inventory_router.get("/", status_code=200)
 async def show_items(response: Response):
-  result = inventory_service.show_events()
-  if "error" in result:
-    response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
-  return result
+  status, message = await inventory_service.show_events()
+  response.status_code = status
+  return message
+  
