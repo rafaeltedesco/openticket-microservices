7d469a2d16bebbe4ddc847a91fc4c347621bdd8c
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2024-02-09 21:42:32 -0300

    feat: create show user usecase

---
+++ users/src/internal/usecases/showusers.go
@@ -0,0 +1,19 @@
+package usecases
+
+import (
+	"openticket/src/internal/models"
+)
+
+type ShowUsersUseCase struct {
+	userModel models.IModelReader
+}
+
+func NewShowUsersUseCase(userModel models.IModelReader) *ShowUsersUseCase {
+	return &ShowUsersUseCase{
+		userModel,
+	}
+}
+
+func (u *ShowUsersUseCase) Execute(page int, pageSize int) (interface{}, error) {
+	return u.userModel.FindAll(page, pageSize)
+}
